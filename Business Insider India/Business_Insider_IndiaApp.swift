3feaa2de0aa5e3eeeb87ba615cb4f9ab63b2c6d9
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-02-08 17:49:51 +0530

    Initial Commit

---
+++ Business Insider India/Business_Insider_IndiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_Insider_IndiaApp.swift
+//  Business Insider India
+//
+//  Created by Vikas Yadav on 08/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Business_Insider_IndiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Business Insider India/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Business Insider India
+//
+//  Created by Vikas Yadav on 08/02/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
