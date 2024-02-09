46251147673392bbfaf7985114404c6f7758cbd6
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-02-09 11:20:39 +0530

    Created the basic code for the Vision Pro - Insider

---
+++ Business Insider India/NavigationView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationView.swift
+//  Business Insider India
+//
+//  Created by Vikas Yadav on 09/02/24.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationView()
+}
---
+++ Business Insider India/TopStory.swift
@@ -0,0 +1,18 @@
+//
+//  TopStory.swift
+//  Business Insider India
+//
+//  Created by Vikas Yadav on 09/02/24.
+//
+
+import SwiftUI
+
+struct TopStory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopStory()
+}
